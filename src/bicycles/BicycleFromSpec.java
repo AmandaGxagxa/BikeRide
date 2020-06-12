678d36d5a558173116a9765f8ec37b02275cc7e0
amandagxagxa <amandagxagxa@gmail.com>
2020-06-12 09:30:31 +0200

    commiting Bicycle specification

--- src/bicycles/BicycleFromSpec.java
+++ src/bicycles/BicycleFromSpec.java
@@ -3,23 +3,23 @@ package bicycles;
 public class BicycleFromSpec extends BicycleBase {
     private int brakeSpeed;
    private int accelerationSpeed;
-   private BicycleType bicycleType;
+   //private BicycleType bicycleType;
 
     public BicycleFromSpec(BicycleSpecification roadBikeSpec) {
         this.accelerationSpeed=roadBikeSpec.getAccelerationSpeed();
         this.brakeSpeed = roadBikeSpec.getBrakeSpeed();
-        this.bicycleType= bicycleType
+       // this.bicycleType= bicycleType
         
     }
 
     @Override
     public void accelerate( ) {
-        speedChange(5);
+        speedChange(accelerationSpeed);
     }
 
     @Override
     public void brake( ) {
-        speedChange(-4);
+        speedChange(brakeSpeed);
 
     }
 
@@ -27,4 +27,14 @@ public class BicycleFromSpec extends BicycleBase {
     public int ride(Bicycle bicycle) {
         return Bicycle.speed;
     }
+    public static void main(String[] args) {
+        BicycleSpecification roadBikeSpec = new BicycleSpecification(11, 4);
+        Bicycle bicycle = new BicycleFromSpec(roadBikeSpec);
+        System.out.println(bicycle.currentSpeed());
+//        Bicycle bicycle = new RoadBike();
+//        bicycle.accelerate();
+//        bicycle.brake();
+//        bicycle.currentSpeed();
+    }
+
 }
